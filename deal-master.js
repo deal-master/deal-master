aaa22d3266a42840b3036345261c65711388e8b2
humanbeing <egeatmaca98@gmail.com>
2021-12-09 14:56:57 +0100

    website

---
+++ deal-master.js
@@ -0,0 +1,21 @@
+function copyToClipboard() {
+    var code = document.body.appendChild(document.createElement("input"));
+    code.value = document.getElementById("wolt-code").innerHTML;
+    code.focus();
+    code.select();
+    document.execCommand('copy');
+    code.parentNode.removeChild(code);
+    document.getElementById('wolt').scrollIntoView();
+    alert("Code copied! 👍");
+    let openWolt = confirm('We are redirecting you to Wolt, so you can get your credits!');
+    if (openWolt) {
+      goToWolt()
+    }
+  }
+
+function goToWolt() {
+  document.getElementById('wolt').scrollIntoView();
+  window.open("https://wolt.com", '_blank');
+}
+
+// 🍔🍕🍟💶
